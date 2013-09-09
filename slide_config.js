0325035a5fe5bbad0c6cb40666338b50163d8e73
Steve Liedtke <sliedtke57@gmail.com>
2013-09-09 14:39:07 +0200

    initial commit

---
+++ slide_config.js
@@ -0,0 +1,20 @@
+var SLIDE_CONFIG = {
+  settings: {
+    title: 'GDG Bremen MONAT 2013',
+    subtitle: "Stammtisch",
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/gdg-bremen/GDG_Bremen_Q4_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['gdgbremen'], 
+  },
+
+  // Author information
+  presenters: [{}]
+};
+
