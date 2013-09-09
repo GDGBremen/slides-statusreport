21b755d77c0484bb46352a45e51da9672fe4aafb
Steve Liedtke <dragondagda@dragondagda-Studio-XPS-1640.(none)>
2013-09-09 22:56:04 +0200

    updated gh-pages through update script

--- scripts/update_slide.sh
+++ scripts/update_slide.sh
@@ -5,5 +5,6 @@ wget https://github.com/GDGBremen/slides-statusreport/archive/$1.zip
 if [ -d ../slides-statusreport-$1 ]
 then
    git rm -r ../slides-statusreport-$1
+   rm -rf ../slides-statusreport-$1
 fi
 unzip $1.zip && rm $1.zip
