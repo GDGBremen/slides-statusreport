cb4e3944a1f204dcd184755722eea0c361e74c4e
Steve Liedtke <dragondagda@dragondagda-Studio-XPS-1640.(none)>
2013-09-10 15:43:24 +0200

    updated gh-pages through update script

--- scripts/update_slide.sh
+++ scripts/update_slide.sh
@@ -5,7 +5,7 @@ wget https://github.com/GDGBremen/slides-statusreport/archive/$1.zip
 pwd
 if [ -d "../slides-statusreport-$1" ]; then
    echo "remove slide folder"
-   git rm -r ../slides-statusreport-$1
+   git rm -r ./slides-statusreport-$1
 else
    echo "no folder slides-statusreport-"$1" found"
 fi
